dff8dece4089bed113754d7955f554e1e42ae634
martin <martinsatakem@gmail.com>
2018-06-22 20:30:14 -0400

    Ajout du formulaire de creation de profil utilisateur

---
+++ src/app/user-profil-form/user-profil-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserProfilFormComponent } from './user-profil-form.component';
+
+describe('UserProfilFormComponent', () => {
+  let component: UserProfilFormComponent;
+  let fixture: ComponentFixture<UserProfilFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UserProfilFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserProfilFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/user-profil-form/user-profil-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profil-form',
+  templateUrl: './user-profil-form.component.html',
+  styleUrls: ['./user-profil-form.component.css']
+})
+export class UserProfilFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
