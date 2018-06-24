796a7442ae2a6e975b6c19c8838504a20db58f7c
Mousselmal Akram <ak4am.mo@gmail.com>
2018-06-24 00:20:33 -0400

    Update server.js path

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/analytiqueFront'));
+app.use(express.static(__dirname + '/dist/e18-analytique'));
 app.listen(process.env.PORT || 8080);
