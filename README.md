5791cc501f92a645fca1bc218ee75107d873f23b
Mousselmal Akram <ak4am.mo@gmail.com>
2018-06-24 00:16:54 -0400

    Fix du package.json

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SignUp
+# Front end Analytique
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.1.
 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/e18-analytique'));
+app.use(express.static(__dirname + '/dist/analytiqueFront'));
 app.listen(process.env.PORT || 8080);
