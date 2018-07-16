83c0e523612b9a174796718db67213cff09c85cc
Akram Mousselmal <ak4am.mo@gmail.com>
2018-07-16 14:41:10 -0400

    add field with token site

--- src/app/components/siteWeb/generateur-code/generateur-code.component.ts
+++ src/app/components/siteWeb/generateur-code/generateur-code.component.ts
@@ -10,6 +10,17 @@ export class GenerateurCodeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  
+  /* To copy Text from Textbox
+     https://stackoverflow.com/questions/49102724/angular-5-copy-to-clipboard
+  */
+  copyInputMessage(inputElement){
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.setSelectionRange(0, 0);
   }
 
 }
